618735c18b790f05b60ff73cc63065d3b05f4dea
Hamed Nikookar <hmdnikookar@gmail.com>
2024-09-17 15:21:44 -0700

    Update aggregate checking code

--- main_check_HN.m
+++ main_check_HN.m
@@ -7,30 +7,68 @@ n_agg = length(Aggs);
 
 ii = [];
 
+imgs_binary_new = imgs_binary;
+
 for i = 1 : n_agg
-
-    fprintf('Image ID: %d \n', Aggs(i).img_id)
-    fprintf('Aggregate ID: %d \n', Aggs(i).id);
-
-    j = i;
-    while isempty(Aggs(j).image)
-        j = j - 1;
-    end
-    
-    
-    tools.imshow_binary(Aggs(j).image, Aggs(i).binary);
-
-    % Prompt user for feedback
-    response = input('Are you satisfied with the binary image? (y/n): ', 's');
-    
-    if strcmpi(response, 'n')
-        ii = [ii, j];
-    end
 
     close all
 
-end
-    
-ii=unique(ii)
+    fprintf('Image ID: %d, ', Aggs(i).img_id)
+    fprintf('Aggregate ID: %d \n', Aggs(i).id);
 
+    n_agg_i = 1;
+    j = i;
+    while isempty(Aggs(j).image)
+        j = j - 1;
+        n_agg_i = n_agg_i + 1;
+    end
+
+
+    tools.imshow_binary(Aggs(j).image, Aggs(i).binary);
+    title(sprintf('Image ID: %d, Aggregate ID: %d', Aggs(i).img_id, Aggs(i).id))
+
+    % Prompt user for feedback on segmentation
+    response1 = input('Are you satisfied with the binary image? (y/n): ', 's');
+
+
+    if strcmpi(response1, 'n') || strcmpi(response1, 'N')
+        ii = [ii, i];
+        imgs_binary_new(Aggs(i).img_id) = agg.seg_slider(imgs(Aggs(i).img_id),...
+            imgs_binary(Aggs(i).img_id));
+
+    elseif ~(strcmpi(response1, 'y') || strcmpi(response1, 'Y'))
+        warning('Invalid resposne! Iterating again...')
+        i = i - 1;
+
+    end
+
+    if (i == n_agg) || (Aggs(i).img_id ~= Aggs(i+1).img_id)
+        fprintf('Number of aggregates in images is: %d\n', n_agg_i);
+        response2 = input('Confirm? (y/n): ', 's');
+    else
+        response2 = 'y';
+    end
+
+    chk = true;
+    while (chk)
+        if strcmpi(response2, 'n') || strcmpi(response2, 'N')
+            imgs_binary_new(Aggs(i).image_id) = agg.seg_slider(imgs(Aggs(i).image_id),...
+                imgs_binary(Aggs(i).image_id));
+
+            chk = false;
+
+        elseif strcmpi(response2, 'y') || strcmpi(response2, 'Y')
+            chk = false;
+
+        else
+            warning('Invalid resposne! Try again...')
+            response2 = input('Confirm now? (y/n): ', 's');
+
+        end
+    end
+
+end
+
+Aggs_new = morph.analyze_binary_HN(imgs_binary_new, pixsizes, imgs, fname);
+close all
 
